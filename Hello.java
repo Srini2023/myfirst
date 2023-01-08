cab8a9a4968b13cc7983b33a6fddfd11094a9130
Srinivas <troubleshootingandtweaks@gmail.com>
2023-01-08 15:28:06 +0000

    committing in child branch

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("hello happy new year 2023 from central repo ");
+System.out.println("hello happy new year 2023 from central repo child branch");
 }
 }
 
